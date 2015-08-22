db9ecc1e50dac3dfb2d85ef0efe030ea8c644c3d
Sam Warren <samwarren909@gmail.com>
2015-08-22 14:38:36 +0100

    #SW Added hello world handlebars layout and view

--- app/app.js
+++ app/app.js
@@ -1,11 +1,23 @@
 var koa = require('koa'),
     app = koa(),
-    port;
+    port,
+    handlebars = require("koa-handlebars");
 
-port = 3000;
+require('dotenv').load();
+
+port = process.env.PORT;
+
+app.use(handlebars({
+    layoutsDir: 'app/layouts',
+    viewsDir: 'app/views',
+    defaultLayout: 'index'
+}));
 
 app.use(function *() {
-  this.body = 'Hello World';
+  yield this.render('index', {
+    title: 'Test Page',
+    name: 'World'
+  });
 });
 
 console.log('Application started on port: ' + port);
